b5d121ddbfb601614401cb68f75154af6cbd6cb2
Jimmy Pautz <jimmy@jimmypautz.com>
2026-08-31 23:24:50 -0500

    Release 26.8.31

--- sink.koplugin/_meta.lua
+++ sink.koplugin/_meta.lua
@@ -5,5 +5,5 @@ return {
     fullname = _("Sink"),
     description = _("Non-intrusive private KOReader progress synchronization for Kindle and other devices with Cloudflare Workers."),
     category = "sync",
-    version = "1.0.0",
+    version = "26.8.31",
 }
